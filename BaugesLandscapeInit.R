e8a07c5d140fd292b5300484ced06eddc1929228
Raphaël Aussenac <raphael.aussenac@gmail.com>
2020-10-07 11:20:02 +0200

    create initial spatial data

---
+++ BaugesLandscapeInit.R
@@ -0,0 +1,77 @@
+###############################################################
+# initialisation
+###############################################################
+
+# clean up environment
+rm(list = ls())
+
+# load packages
+library(rgdal)
+library(raster)
+library(velox)
+library(gdalUtils)
+library(sf)
+
+# set work directory
+setwd("C:/Users/raphael.aussenac/Documents/GitHub/LandscapeInit")
+
+###############################################################
+# load/create data and set extent and resolution
+###############################################################
+
+# park
+park <- readOGR(dsn = "./data/GEO", layer = "park", encoding = "UTF-8", use_iconv = TRUE)
+
+# forest
+forest <- readOGR(dsn = "./data/GEO", layer = "BD_Foret_V2_PNRfilled_Foret_2014", encoding = "UTF-8", use_iconv = TRUE)
+
+# elevation
+elevation <- raster("./data/GEO/MNT_all_5m.tif")
+# set projection
+crs(elevation) <- crs(park)
+# set resolution
+elevation <- aggregate(elevation, fact = 5)
+
+# slope
+slope <- terrain(elevation, opt = 'slope', unit = 'degrees', neighbors = 8)
+
+# aspect
+aspect <- terrain(elevation, opt = 'aspect', unit = 'degrees', neighbors = 8)
+
+# convert park into raster and set extent + resolution
+ext <- floor(extent(elevation))
+r <- raster(ext, res=res(elevation))
+parkRaster <- rasterize(park, r, field = "ID")
+# set projection
+crs(parkRaster) <- crs(park)
+# convert NA into 0
+isBecomes <- cbind(c(1, NA),
+                   c(1, 0))
+parkRaster <- reclassify(parkRaster, rcl = isBecomes)
+
+# convert forest into raster and set extent + resolution
+forestRaster <- rasterize(forest, r, field = "ID")
+# set projection
+crs(forestRaster) <- crs(forest)
+# convert NA into 0
+isBecomes <- cbind(c(1:nrow(forest), NA),
+                   c(rep(1, nrow(forest)), 0))
+forestRaster <- reclassify(forestRaster, rcl = isBecomes)
+
+# swhc
+soil <- raster("./data/GEO/rum_500_v2009.tif")
+soil <- resample(soil, elevation)
+
+###############################################################
+# save ascii
+###############################################################
+
+# create directory to save plots
+if (!(dir.exists('Init'))) {dir.create('Init', recursive = TRUE)}
+
+writeRaster(parkRaster, filename = "./Init/park.asc", format = "ascii", datatype = 'INT4S', overwrite = TRUE)
+writeRaster(forestRaster, filename = "./Init/forest.asc", format = "ascii", datatype = 'INT4S', overwrite = TRUE)
+writeRaster(elevation, filename = "./Init/elev.asc", format = "ascii", datatype = 'INT4S', overwrite = TRUE)
+writeRaster(slope, filename = "./Init/slope.asc", format = "ascii", datatype = 'INT4S', overwrite = TRUE)
+writeRaster(aspect, filename = "./Init/aspect.asc", format = "ascii", datatype = 'INT4S', overwrite = TRUE)
+writeRaster(soil, filename = "./Init/soil.asc", format = "ascii", datatype = 'INT4S', overwrite = TRUE)
