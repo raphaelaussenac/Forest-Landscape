f97b7d5f592a243a6d9ebbd665d77bdf9a9419b3
Raphaël Aussenac <raphael.aussenac@gmail.com>
2020-11-25 22:24:55 +0100

    remove trees <7.5 and evaluate algo biases

--- dendro.R
+++ dendro.R
@@ -9,6 +9,8 @@ rm(list = ls())
 library(dplyr)
 library(raster)
 library(doParallel)
+library(ggplot2)
+library(tidyr)
 
 # set work directory
 setwd("C:/Users/raphael.aussenac/Documents/GitHub/LandscapeInit")
@@ -30,6 +32,7 @@ Dprop <- Dprop / 100
 # create raster stack
 rasterStack <- stack(compo, Dg, BA, Dprop)
 rasterStack$cellID <- c(1:nrow(rasterStack[]))
+writeRaster(rasterStack$cellID, filename = "./data/Init/cellID.asc", format = "ascii", overwrite = TRUE)
 
 # load NFI tree data
 tree <- read.csv('./data/NFI/arbres_Bauges_2020_10_15.csv', sep = ';')
@@ -65,7 +68,7 @@ NFIsp <- tree %>% group_by(idp, species_name, BAsp, Dgsp, spType, spPropdc) %>% 
 ###############################################################
 
 # rasterStack <- crop(rasterStack, extent(rasterStack)/5)
-# cell <- rast1[8581]
+# cell <- rast1[800000]
 
 # Split data into 4 separate stack rasters
 xmin <- extent(rasterStack)[1]
@@ -166,8 +169,7 @@ assignDendro <- function(cell, i, tree, NFIsp){
     dbh$wlid <- 40000 / pi * dbh$BAtreelid / dbh$DBHlid^2
 
     # calculate round(weight) for a 25*25m pixel
-    dbh$w25m <- round(dbh$wlid/16)
-    # set min weight to 1
+    # and set min weight to 1
     dbh$w25m <- apply(as.data.frame(dbh[, 'wlid']), 1, function(x) max(1, round(x/16)))
 
     # assign new dbh to trees while keeping BAtreelid
@@ -226,9 +228,8 @@ results3$i <- results3$i + max(results2$i)
 results4$i <- results4$i + max(results3$i)
 results <- rbind(results1, results2, results3, results4)
 
-
 # remove trees with sp = XXX and n = dbh = NA
-# only if it does not remove the cell
+# but only if it does not remove the cell
 # these 'strange' trees are created because they are deci or coni on cells
 # with dprop = 0 or 1
 # first count number of cells with composition = XXX and with all trees dbh = NA
@@ -243,27 +244,48 @@ if (nrow(df2[df2$N == 0,]) == 0){
 write.csv(results[, c('cellID', 'sp', 'n', 'dbh')], file = './data/Init/trees.csv', row.names = FALSE)
 
 ################################################################################
-# measure the effect of rounding on number of trees
+# evaluate the effect of rounding on the number of trees
 ################################################################################
 
-# distribution of differences at the cell level
+# distribution of differences between rounded and non-rounded number of trees
+# at the cell level
 Ncell <- results %>% group_by(cellID) %>% summarise(wlid = sum(wlid), n = sum(n)) %>%
                      mutate(Ndiff = wlid - n)
-pdf(file="./data/Init/NdiffCell.pdf")
-hist(Ncell$Ndiff, xlab = 'N diff at the cell level (positive values = underestimation)', breaks = 100, main = '')
-dev.off()
+pl1 <- ggplot()+
+geom_histogram(data = Ncell, aes(x = Ndiff), bins = 50, col = 'black', fill = 'white', alpha = 0.5) +
+theme_bw() +
+xlab('N diff at the cell level (negative values = overestimation)')
+pl1
+ggsave(file = './data/Init/NdiffCell.pdf', plot = pl1, width = 10, height = 10)
 
-# distribution of differences at the tree level
-Nresults <- results
-Nresults$diff <- Nresults$wlid - Nresults$n
-pdf(file="./data/Init/NdiffTree.pdf")
-hist(Nresults$diff, xlab = 'N diff at the tree level (positive values = underestimation)', breaks = 100, main = '')
-dev.off()
+# distribution of differences between rounded and non-rounded number of trees per species
+# at the cell level depending on dbh classes
+sp <- results %>% group_by(cellID, sp) %>% summarise(SpMeanDBH = sum(n * dbh) / sum(n), SpNDiff = sum(wlid) - sum(n))
+sp$meanDBHclass <- cut(sp$SpMeanDBH, 0:120)
+pl2 <- ggplot() +
+geom_boxplot(data = sp, aes(x = meanDBHclass, y = SpNDiff), alpha = 0.5) +
+theme_bw() +
+theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+xlab('species mean dbh in cells') +
+ylab('species N diff at the cell level (negative values = overestimation)')
+pl2
+ggsave(file = './data/Init/NdiffDBH.pdf', plot = pl2, width = 30, height = 20)
 
-# difference and dbh
-pdf(file="./data/Init/NdiffDBH.pdf")
-plot(Nresults$diff ~ Nresults$dbh, xlab = 'dbh', ylab = 'N diff (positive values = underestimation)', main = '')
-dev.off()
+# distribution of differences between rounded and non-rounded number of trees per species
+# at the cell level depending on dbh classes separately for each species
+pl3 <- ggplot() +
+geom_boxplot(data = sp, aes(x = meanDBHclass, y = SpNDiff), alpha = 0.5) +
+facet_wrap(~ sp) +
+theme_bw() +
+theme(panel.grid.minor = element_blank(),
+      strip.background = element_blank(),
+      strip.text = element_text(colour = 'black'),
+      axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+      axis.text=element_text(size = 3)) +
+xlab('species mean dbh in cells') +
+ylab('species N diff at the cell level (negative values = overestimation)')
+pl3
+ggsave(file = './data/Init/NdiffDBHSp.pdf', plot = pl3, width = 30, height = 20)
 
 # total over/under estimation of trees at the landscape scale
 # positive values = underestimation
@@ -275,18 +297,88 @@ sum(Ncell$Ndiff, na.rm = TRUE) * 100 /sum(results$n, na.rm = TRUE)
 # check whether BAlidar = sum of BA of trees at each cell
 ################################################################################
 
-BAinit <- results %>% group_by(i) %>% summarise(BAtot = sum((pi * (dbh/200)^2) * n * 16)) %>% arrange(i)
+BAinit <- results %>% group_by(cellID) %>% summarise(BAtot = sum((pi * (dbh/200)^2) * n * 16)) %>% arrange(cellID)
 rasterStack$BAinit <- BAinit$BAtot
-rasterStack$diff <- rasterStack$BA - rasterStack$BAinit
-plot(rasterStack, col = gray.colors(10, start = 0.3, end = 0.9, gamma = 2.2, alpha = NULL))
-plot(rasterStack$BA, col = gray.colors(10, start = 0.3, end = 0.9, gamma = 2.2, alpha = NULL))
-plot(rasterStack$BAinit, col = gray.colors(10, start = 0.3, end = 0.9, gamma = 2.2, alpha = NULL))
-plot(rasterStack$compoID, col = gray.colors(10, start = 0.3, end = 0.9, gamma = 2.2, alpha = NULL))
+rasterStack$BAdiff25m <- (rasterStack$BA / 16)  - (rasterStack$BAinit /16)
+rasterStack$BAreldiff25m <- 100 - ( (rasterStack$BAinit /16) * 100 / (rasterStack$BA / 16) )
 
-# save results
-pdf(file="./data/Init/BAobsPredDiff.pdf")
-plot(rasterStack$diff, legend = TRUE)
+# difference should be as close to zero as possible
+pdf(file="./data/Init/BAdiff25m.pdf")
+hist(rasterStack$BAdiff25m, breaks = 100)
 dev.off()
+plot(rasterStack$BAdiff25m)
+pdf(file="./data/Init/BAreldiff.pdf")
+hist(rasterStack$BAreldiff25m, breaks = 100)
+dev.off()
+plot(rasterStack$BAreldiff25m)
+
+################################################################################
+# check whether Dglidar = mean Dg of trees at each cell
+################################################################################
+
+Dginit <- results %>% group_by(cellID) %>% summarise(Dgtot = sqrt(sum(dbh^2 * n)/sum(n))) %>% arrange(cellID)
+rasterStack$Dginit <- Dginit$Dgtot
+rasterStack$Dgdiff <- rasterStack$dg - rasterStack$Dginit
+rasterStack$Dgreldiff <- 100 - (rasterStack$Dginit * 100 /rasterStack$dg)
+
+# difference should be as close to zero as possible but it necessarily
+# fluctuates because we had to change the trees dbh to reach the BA
+# prescribed by the LIDAR
+pdf(file="./data/Init/Dgdiff.pdf")
+hist(rasterStack$Dgdiff, breaks = 100)
+dev.off()
+plot(rasterStack$Dgdiff)
+pdf(file="./data/Init/Dgreldiff.pdf")
+hist(rasterStack$Dgreldiff, breaks = 100)
+dev.off()
+plot(rasterStack$Dgreldiff)
+# writeRaster(rasterStack$Dgreldiff, filename = "./data/Init/rasterVerif.asc", format = "ascii", overwrite = TRUE)
+# rasterStack[802041]
+# results[results$cellID == 802041,]
+# saveResults[saveResults$cellID == 802041,]
+
+# check forest surface
+sum(!is.na(rasterStack$dg[])) * 625 / 10000
+sum(!is.na(rasterStack$compoID[])) * 625 /10000
+sum(!is.na(rasterStack$BAinit[])) * 625 / 10000
+sum(!is.na(rasterStack$Dginit[])) * 625 / 10000
+
+################################################################################
+# remove trees <7.5 cm
+################################################################################
+
+saveResults <- results
+
+# remove trees <7.5cm in cells where there are some trees left
+# otherwise, if all trees are <7.5 then replace by a line of NA
+
+# first count total number of lines per cell, number of line with dbh <7.5
+# and number with dbh >= 7.5
+treeCnt <- results %>% filter(!is.na(sp)) %>% mutate(n = 1, size = ifelse(dbh >= 7.5, 'big', 'small')) %>%
+                   group_by(cellID) %>% count(size, wt = n) %>% pivot_wider(id_cols = cellID, names_from = size, values_from = n) %>%
+                   mutate(nbtot = sum(big, small, na.rm = TRUE))
+# list of cells where small trees can (simply) be removed
+removelist <- treeCnt %>% filter(small > 0 & !is.na(small) & big > 0 & !is.na(big))
+removelist <- as.list(removelist[, 'cellID'])
+
+# list of cells where small trees make up the whole stand
+# and cannot be removed --> create a NA line instead
+setToNAlist <- treeCnt %>% filter(small > 0 & !is.na(small) & is.na(big))
+setToNAlist <- as.list(setToNAlist[, 'cellID'])
+
+# remove trees
+results <- results %>% filter(!( cellID %in% removelist$cellID & dbh <7.5))
+
+# set to NA
+# for that we remove the cells and add new NA lines
+results <- results %>% filter(! (cellID %in% setToNAlist$cellID))
+NAdf <- data.frame(sp = NA, wlid = NA, n = NA, dbh = NA, i = NA, cellID = setToNAlist$cellID)
+results <- rbind(results, NAdf)
+results <- results %>% arrange(cellID)
+
+# results[results$dbh < 7.5 & !is.na(results$dbh), c('sp', 'wlid', 'n', 'dbh')] <- NA
+write.csv(results[, c('cellID', 'sp', 'n', 'dbh')], file = './data/Init/trees75.csv', row.names = FALSE)
+
 
 # verif lab !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # verifier si les BA calculer a partir des dbh lidar finaux correspondent bien
