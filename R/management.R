d7d17ab102d6bbff9fd44926646943b1283ab21f
Raphaël Aussenac <raphael.aussenac@gmail.com>
2021-07-05 20:43:31 +0200

    define composition type in management table

--- R/management.R
+++ R/management.R
@@ -7,6 +7,8 @@ require(reldist)
 require(dplyr)
 require(rgdal)
 require(raster)
+require(ggplot2)
+library(stringr)
 
 # load virtual tree data
 tree <- read.csv(paste0(landPath, '/trees75.csv'))
@@ -97,12 +99,126 @@ df <- merge(df, protect, by = 'cellID100')
 # define composition type
 ###############################################################
 
---> prendre script composition dans evalcompo
-- beech
-- fir and/or spruce
-- mixed beech - fir and/or spruce
-- other deciduous
----- ajouter oak?
+# set threshold to identify mainSp
+# thresh = 0.8 means you will get the species making up for >= 80% of
+# the stand BA
+thresh = 0.70
+# retrieve main species
+mainSp <- tree %>% group_by(cellID100, sp) %>%
+                          summarise(BA = sum((pi * (dbh/200)^2) * n)) %>%
+                          group_by(cellID100) %>% arrange(cellID100, -BA) %>%
+                          mutate(BAtot = sum(BA), BAprop = BA/BAtot, cumulProp = cumsum(BAprop), HigherThanthresh = case_when(
+                            cumulProp >= thresh ~ cumulProp), minCompo = min(HigherThanthresh, na.rm = TRUE)) %>%
+                          filter(cumulProp <= minCompo) %>% summarise(mainSp = paste(sp, collapse=' - '))
+#
+
+# class main species into composition types:
+# deciduous and coniferous sp list----------------------------------------------
+deciduousSp <- c('Fagus sylvatica', 'Sorbus aria', 'Quercus robur',
+                 'Quercus petraea', 'Acer opalus', 'Tilia platyphyllos',
+                 'Carpinus betulus', 'Castanea sativa', 'Populus nigra',
+                 'Fraxinus excelsior', 'Corylus avellana', 'Acer campestre',
+                 'Betula pendula', 'Populus tremula', 'Prunus avium',
+                 'Acer pseudoplatanus', 'Tilia cordata', 'Salix caprea',
+                 'Ulmus glabra', 'Sorbus aucuparia', 'Robinia pseudacacia',
+                 'Malus sylvestris', 'Acer platanoides', 'Salix alba',
+                 'Crataegus monogyna', 'Sorbus mougeotii', 'Laburnum anagyroides',
+                 'Quercus pubescens', 'Alnus glutinosa', 'Salix cinerea',
+                 'Alnus incana', 'Ilex aquifolium', 'Buxus sempervirens',
+                 'Prunus padus', 'Robinia pseudoacacia')
+#
+coniferousSp <- c('Picea abies', 'Abies alba', 'Pinus sylvestris',
+                  'Taxus baccata', 'Larix decidua', 'Larix kaempferi',
+                  'Picea sitchensis')
+#-------------------------------------------------------------------------------
+# identify deciduous and coniferous stands
+mainSp <- mainSp %>% group_by(cellID100) %>% mutate(D = sum(str_detect(mainSp, deciduousSp)),
+                                                    C = sum(str_detect(mainSp, coniferousSp)),
+                                                    compoType = if_else(D>0 & C>0, 'DC', if_else(D>0 & C==0, 'D', 'C')))
+#
+# beech
+mainSp[mainSp$mainSp == 'Fagus sylvatica' , 'compoType'] <- 'beech'
+# fir and/or spruce
+fs <- c('Abies alba', 'Picea abies', 'Abies alba - Picea abies', 'Picea abies - Abies alba')
+mainSp[mainSp$mainSp %in% fs , 'compoType'] <- 'fir and/or spruce'
+# mixed beech - fir and/or spruce
+m <- c('Fagus sylvatica - Abies alba','Abies alba - Fagus sylvatica',
+       'Fagus sylvatica - Picea abies','Picea abies - Fagus sylvatica',
+       'Fagus sylvatica - Abies alba - Picea abies',
+       'Fagus sylvatica - Picea abies - Abies alba',
+       'Abies alba - Picea abies - Fagus sylvatica',
+       'Abies alba - Fagus sylvatica - Picea abies',
+       'Picea abies - Fagus sylvatica - Abies alba',
+       'Picea abies - Abies alba - Fagus sylvatica')
+mainSp[mainSp$mainSp %in% m , 'compoType'] <- 'beech with fir and/or spruce'
+
+# add to df
+df <- merge(df, mainSp[, c('cellID100', 'compoType')], by = 'cellID100', all.x = TRUE)
+
+# plot composition types proportion in landscape
+compo <- mainSp %>% group_by(compoType) %>% mutate(surface = 1) %>%
+                    summarise(surface = sum(surface)) %>% arrange(-surface) %>%
+                    ungroup() %>% mutate(totSurf = sum(surface)) %>%
+                    group_by(compoType) %>% mutate(relSurf = surface * 100 / totSurf) %>%
+                    dplyr::select(-totSurf)
+compo$compoType <- factor(compo$compoType, levels = compo$compoType)
+
+# plot composition at 1ha cells
+ggplot(data = compo) +
+geom_bar(aes(x = compoType, y = surface), stat = 'identity') +
+geom_text(aes(x = compoType, y = surface, label = paste(round(relSurf, 2), '%') ), vjust = -0.3, size = 5, col = 'orange') +
+geom_text(aes(x = compoType, y = surface, label = paste(round(surface, 2), 'ha') ), vjust = +1.2, size = 5, col = 'orange') +
+theme_light() +
+xlab('main species') +
+ylab('surface (ha)') +
+theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1),
+     plot.title = element_text(hjust = 0.5))
+#
+
+# TODO: sortir liste des espèces decidues/coniferes (ici et dans spTransform)
+
+###############################################################
+# define stand type (compoType + even / uneven / protect / ...)
+###############################################################
+
+# assign compoType to stand type
+df$standType <- paste(df$compoType, df$type)
+
+# mark as 'nologging' non-loggable stands
+df[df$loggable == 0 & !is.na(df$loggable), 'standType'] <- paste(df[df$loggable == 0 & !is.na(df$loggable), 'compoType'], 'noLogging')
+
+# mark as 'protected' protected stands
+df[df$protect == 1, 'standType'] <- 'protected'
+
+# plot stand type share in the landscape
+typeShare <- df %>% mutate(surface = 1) %>% group_by(standType) %>% filter(compoType != 'NA') %>%
+                    summarise(surface = sum(surface)) %>% arrange(-surface) %>%
+                    ungroup() %>% mutate(totSurf = sum(surface)) %>%
+                    group_by(standType) %>% mutate(relSurf = surface * 100 / totSurf) %>%
+                    dplyr::select(-totSurf)
+
+typeShare$standType <- factor(typeShare$standType, levels = typeShare$standType)
+#
+ggplot(data = typeShare) +
+geom_bar(aes(x = standType, y = surface), stat = 'identity') +
+geom_text(aes(x = standType, y = surface, label = paste(round(relSurf, 2), '%') ), vjust = -0.3, size = 5, col = 'orange') +
+geom_text(aes(x = standType, y = surface, label = paste(round(surface, 2), 'ha') ), vjust = +1.2, size = 5, col = 'orange') +
+theme_light() +
+xlab('main species') +
+ylab('surface (ha)') +
+theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1),
+     plot.title = element_text(hjust = 0.5))
+#
+
+# plot on map
+cellID100$standType <- as.numeric(as.factor(df$standType))
+plot(cellID100$standType)
+plot(park, add = T)
+
+###############################################################
+# ownership
+###############################################################
+
 
 ###############################################################
 # number of forest 25*25m cells in ha
@@ -115,47 +231,3 @@ df <- merge(df, protect, by = 'cellID100')
 ###############################################################
 
 valeurs sp / feuillus / pins / autres résineux
-
-
-
-###############################################################
-###############################################################
-###############################################################
-
-# lab
-
-hist(protect[protect$protect > 0,])
-
-
-
-plot(rb, col = 'green', border = 'green')
-plot(ri, col = 'red', border = 'red', add =TRUE)
-plot(rn, col = 'blue', border = 'blue', add =TRUE)
-plot(cellID25, add = TRUE)
-
-
-irr <- gini %>% filter(Gini > 0.45)
-reg <- gini %>% filter(Gini <= 0.45)
-
-
-nrow(irr) * 100 /nrow(gini)
-
-83% d'irrégulier
-16% de regulier
-
-
-
-hist(df$Gini, breaks = 100)
-range(df$Gini)
-
-
-
-# library(plotrix)
-weighted.hist(x = test$dbh, w = test$n)
-
-
-8636 -> 0.270
-
-
-test <- tree[tree$cellID100 == 8636, ]
-gini(x = test$dbh, weights = test$n)
