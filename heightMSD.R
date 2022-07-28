7678610463b6916e7d910f1e606e623f24ea9c42
Raphaël Aussenac <raphael.aussenac@proton.me>
2022-07-28 16:34:14 +0200

    msd figure for article

--- heightMSD.R
+++ heightMSD.R
@@ -6,11 +6,12 @@
 rm(list = ls())
 
 # set work directory
-setwd('./Documents/github/Forest-Landscape/evalHeight')
+setwd('./Documents/code/Forest-Landscape/evalHeight')
 
 # load library
 library(ggplot2)
 library(dplyr)
+library(stringr)
 
 # load msd data
 bauges <- read.csv('bauges_msd.csv')
@@ -21,14 +22,33 @@ sneznik <- read.csv('sneznik_msd.csv')
 df <- bind_rows(bauges, milicz, sneznik) %>% filter(var != 'msd')
 df$landscape <- factor(df$landscape, levels = c('bauges', 'sneznik', 'milicz'))
 
+# format
+df$var <- toupper(df$var)
+df$var <- factor(df$var, levels = c('LC', 'NU', 'SB'))
+df$landscape <- str_to_title(df$landscape)
+df$landscape <- factor(df$landscape, levels = c('Bauges', 'Sneznik', 'Milicz'))
+
+
 # plot
 pl1 <- ggplot(df) +
 geom_bar(aes(x = landscape, y = components, fill = var), stat = 'identity', position = 'stack') +
-ylab('mean square deviation') +
+scale_fill_grey(start = 0.2, end = 0.9) +
+# ylab('mean square deviation') +
+# xlab('') +
 theme_bw() +
-theme(legend.position = 'bottom') +
-guides(fill = guide_legend(title="MSD components"))
-ggsave(file = './msd.pdf', plot = pl1, width = 5, height = 10)
+scale_y_continuous(expand = c(0, 0)) +
+scale_x_discrete(expand = c(0.25, 0)) +
+theme(legend.position = 'bottom', panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.border = element_blank(),
+      axis.ticks.x = element_blank(),
+      # axis.line.x = element_line(),
+      # axis.line.y = element_line()
+      axis.title.y = element_blank(),
+      axis.title.x = element_blank()) +
+guides(fill = guide_legend(title=""))
+ggsave(file = './msd.pdf', plot = pl1, width = 2, height = 4)
+pl1
 
 # plot relative msd
 dfFreq <- df %>% group_by(landscape, var) %>%
