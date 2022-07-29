4a929230a8a0ad554753ff6c889cc3388c023029
Raphaël Aussenac <raphael.aussenac@proton.me>
2022-07-29 17:52:20 +0200

    correct Hdom calculation

--- R/evalHeight.R
+++ R/evalHeight.R
@@ -9,6 +9,7 @@ evalHeight <- function(landscape){
   require(dplyr)
   require(ggplot2)
   require(ggExtra)
+  require(stringr)
 
   # load LiDAR heights
   lidH <- list.files(paste0('./data/', landscape, '/LiDARh'), pattern = '.rda')
@@ -19,6 +20,7 @@ evalHeight <- function(landscape){
 
   # load cellID25 raster
   cellID25 <- raster(paste0(landPath, '/cellID25.asc'))
+  names(cellID25) <- 'cellID25'
 
   # merge data sets
   # stack cellID25 and LiDAR heights
@@ -34,9 +36,15 @@ evalHeight <- function(landscape){
 
   # calculate mean height of n highest trees
   compareH <- function(nbTrees, tree){
-    compH <- tree %>% group_by(cellID25) %>% arrange(-cellID25, -h) %>% slice(1:nbTrees) %>%
-                      mutate(nh = n()) %>%
-                      summarise(h = mean(h), hlid = unique(get(paste0('Hm', nbTrees))))
+    compH <- tree %>% group_by(cellID25) %>%
+                      arrange(-cellID25, -h) %>%
+                      mutate(cumN = cumsum(n)) %>%
+                      mutate(NN = ifelse(cumN<=nbTrees, 0, cumN-nbTrees)) %>%
+                      mutate(n2 = abs(n - NN)) %>%
+                      mutate(cumN2 = cumsum(n2)) %>%
+                      filter(cumN2 <= nbTrees) %>%
+                      select(-n, cumN, -NN, -cumN2) %>%
+                      summarise(h = weighted.mean(h, n2), hlid = unique(get(paste0('Hm', nbTrees))))
     return(compH)
   }
   heights <- compareH(6, tree)
@@ -52,17 +60,20 @@ evalHeight <- function(landscape){
   geom_point(alpha = 0, size = 0.5, col = 'grey', pch = 16) +
   xlim(5, 45) +
   ylim(5, 45) +
+  labs(y = expression(Hdom[L])) +
+  labs(x = expression(Hdom[T])) +
   coord_fixed() +
-  annotate(geom = 'text', x = 10, y = 40, label = paste('r² = ', round(summary(mod)$r.squared,2)), col = 'red', size = 10) +
-  ggtitle(landscape) +
-  geom_boxplot(aes(x = class, y = hlid, group = class), alpha = 0.8) +
+  annotate(geom = 'text', x = 15, y = 40, label = paste('R² = ', round(summary(mod)$r.squared,2)), col = 'red', size = 5) +
+  ggtitle(str_to_title(landscape)) +
+  geom_boxplot(aes(x = class, y = hlid, group = class), alpha = 0.8, outlier.shape = NA) +
   geom_abline(intercept = 0, slope = 1, color = "black", linetype = 2, size = 1) +
   geom_abline(intercept = mod$coef[1], slope = mod$coef[2], color = "red", linetype = 1, size = 1) +
-  theme_bw() +
+  # theme_bw() +
+  theme_classic() +
   theme(plot.title = element_text(hjust = 0.5))
-  pl1 <- ggMarginal(pl1, type = 'histogram', margin = 'x', size = 7, xparams = list(fill = "grey", bins = 100))
-  pl1
-  ggsave(file = paste0(evalHeightPath, '/', landscape, '_evalHeight.jpg'), plot = pl1, width = 10, height = 10)
+  pl1 <- ggMarginal(pl1, type = 'histogram', margin = 'x', size = 7, xparams = list(fill = "white", bins = 100))
+  # pl1
+  ggsave(file = paste0(evalHeightPath, '/H', landscape, '.pdf'), plot = pl1, width = 7, height = 7)
 
 
   ###############################################################
